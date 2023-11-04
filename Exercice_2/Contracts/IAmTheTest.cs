aabe58bfd0d81f5df4b685a70c3690848b967e7d
Stéphane Adjakotan <stephane.adjakotan@gmail.com>
2023-11-04 19:52:43 +0100

    Création de l'interface IAmTheTest

---
+++ Exercice_2/Contracts/IAmTheTest.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercice_2.Contracts
+{
+    public interface IAmTheTest
+    {
+        bool CheckSirenValidity(string siren);
+        string ComputeFullSiren(string sirenWithoutControlNumber);
+    }
+}
